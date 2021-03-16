c6993b393c74899f5bf9dba4166953496fea6f7c
Rohit <khandarerohitrk@gmail.com>
2021-03-16 15:14:56 +0530

    fixed

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Constants {
 	public static final String LOGIN_PAGE_TITLE ="Account Login";
 	public static final String ACCOUNT_PAGE_TITLE ="My Account";
-	public static final int ACCOUNT_PAGE_SECTIONS =5;
+	public static final int ACCOUNT_PAGE_SECTIONS =4;
 	public static final int PRODUCT_IMAGES_COUNT_FOR_MACBOOK =4;
 	public static final String SHOPPING_CART_PAGE_TITLE="Shopping Cart";
 	public static final String ACCOUNT_CREATION_SUCCESS_MSG="Your Account Has Been Created!";
