ae957b85ba2d801fe7ec77407cbeb341d52f579a
Rohit <khandarerohitrk@gmail.com>
2021-03-16 18:46:21 +0530

    added8

--- src/main/java/com/qa/opencart/utils/TestUtility.java
+++ src/main/java/com/qa/opencart/utils/TestUtility.java
@@ -6,7 +6,7 @@ public class TestUtility {
 	
 	public static String getRandomEmail() {
 		Random numgenerator = new Random();
-		String email = "testauto"+ numgenerator.nextInt(5000)+"@gmail.com";
+		String email = "testautone"+ numgenerator.nextInt(5000)+"@gmail.com";
 		return email;
 	}
 }
